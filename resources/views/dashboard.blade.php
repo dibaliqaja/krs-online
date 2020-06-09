195a63006d79af3d0876d3fc26b91b079aa379c1
Iqbal <dibaliqaja@gmail.com>
2020-06-10 06:29:11 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.home')
+@section('title_page','Dashboard')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-info">
+                    <i class="fas fa-file-signature"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Provinces</h4>
+                    </div>
+                    <div class="card-body">
+                        {{-- {{ DB::table('provinces')->count() }} --}}20
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-info">
+                    <i class="fas fa-file-signature"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Cities</h4>
+                    </div>
+                    <div class="card-body">
+                        {{-- {{ DB::table('cities')->count() }} --}}30
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-info">
+                    <i class="fas fa-file-signature"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Areas</h4>
+                    </div>
+                    <div class="card-body">
+                        {{-- {{ DB::table('areas')->count() }} --}}33
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
